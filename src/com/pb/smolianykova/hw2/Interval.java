e477d4bf3559899558a48f77fc58cfb02c818143
Vladlena Smolianykova <vladlena.smolianykova@privatbank.ua>
2021-10-17 00:32:45 +0300

    init commit

--- src/com/pb/smolianykova/hw2/Interval.java
+++ src/com/pb/smolianykova/hw2/Interval.java
@@ -1,19 +1,28 @@
 package com.pb.smolianykova.hw2;
 
+
 import java.util.Scanner;
 
 public class Interval {
-    private static Scanner scan;
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите значение переменной: ");
-        int x = scan.nextInt();
-        if (х >= 0) {
-        System.out.println("диапозон [0 -14]");
+        Scanner scan = new Scanner(System.in);
+        int x;
+        System.out.println("Введите число от 0 до 100");
+        x = scan.nextInt();
+        if (x==0 || x<100) {
+            if (x<=14) {
+                System.out.println("диапозон [0-14]");
+            }else if (x>=15 || x <=35) {
+                System.out.println("диапозон [15-35]");
+            }else if (x>=36 || x <=50) {
+                System.out.println("диапозон [36-50]");
+            }else if (x>=51 || x <=100) {
+                System.out.println("диапозон [50-100]");}
 
+            }
+        }
     }
-    }
-}
+
+
 
 
