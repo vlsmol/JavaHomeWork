c86ee85cd0322090f3827cc0fd6572118a041d05
Vladlena Smolianykova <vladlena.smolianykova@privatbank.ua>
2021-10-25 12:20:15 +0300

    init commit

--- src/com/pb/smolianykova/hw3/Array.java
+++ src/com/pb/smolianykova/hw3/Array.java
@@ -1,37 +1,26 @@
 package com.pb.smolianykova.hw3;
 
 import java.util.Arrays;
-import java.util.Random;
 
 public class Array {
-
-
-    private static int temp;
-
     public static void main(String[] args) {
-        int[] array = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 16};
-        int mas = array.length;
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i] + "");
-            System.out.println("введите целое число");
+        int [] mas = {11, 3, 14, 16, 7, 9, 5, 8, 2, 4};
 
-    }
-        int sum = Arrays.stream(array).sum();
-        System.out.println("сумма целых чисел" + sum);
-        int positive= 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > 0)
-                positive ++;
-            System.out.println("количество цифр больше ноля = "+i);
-            boolean isSorted = false;
-            while (!isSorted) {
-                isSorted = true;
-                for (i = 0; i < array.length; i++)
-                    if (array[i] > array[i + 1])
-                array[i + 1] = temp;
-                isSorted = false;
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < mas.length-1; i++) {
+                if(mas[i] > mas[i+1]){
+                    isSorted = false;
+
+                    buf = mas[i];
+                    mas[i] = mas[i+1];
+                    mas[i+1] = buf;
+                }
 
             }
         }
-}
+        System.out.println(Arrays.toString(mas));
+    }
 }
