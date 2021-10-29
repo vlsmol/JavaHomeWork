e9d7ae392d7002fc2812ef24fa2b0b9eacd2fdde
Vladlena Smolianykova <vladlena.smolianykova@privatbank.ua>
2021-10-29 22:09:08 +0300

    init commit

--- src/com/pb/smolianykova/hw3/Array.java
+++ src/com/pb/smolianykova/hw3/Array.java
@@ -1,26 +1,62 @@
 package com.pb.smolianykova.hw3;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        int [] mas = {11, 3, 14, 16, 7, 9, 5, 8, 2, 4};
+        byte SIZE = 10;
+        int[] array = new int[SIZE];
+        boolean isError = false;
+        Scanner in = new Scanner(System.in);
 
-        boolean isSorted = false;
-        int buf;
-        while(!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < mas.length-1; i++) {
-                if(mas[i] > mas[i+1]){
-                    isSorted = false;
-
-                    buf = mas[i];
-                    mas[i] = mas[i+1];
-                    mas[i+1] = buf;
-                }
-
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Введите целое число, " + (i+1) + "-й элемент массива из " + SIZE);
+            try {
+                array[i] = in.nextInt();
+            }catch (Exception e){
+                System.out.println("Вы ввели не целое число");
+                isError = true;
+                break;
             }
         }
-        System.out.println(Arrays.toString(mas));
+        if (!isError) {
+            System.out.print("Введенный массив: " + Arrays.toString(array));
+            System.out.println("\nСумма всех элементов массива: " + sum(array));
+            System.out.println("Количество положительных элементов массива: " + positiveCount(array));
+            System.out.println("Отсортированный массив : " + Arrays.toString(sortArray(array)));
+        }
+        System.out.println("Конец процесса");
+    }
+
+    public static long sum(int[] array) {
+        long sum = 0;
+        for (int el : array) {
+            sum += el;
+        }
+        return sum;
+    }
+
+    public static int positiveCount(int[] array) {
+        int count = 0;
+        for (int el : array) {
+            if (el > 0) count++;
+        }
+        return count;
+    }
+
+    public static int[] sortArray(int[] array) {
+        int to = array.length - 1;
+        for (int i = 0; i < to; i++) {
+            int temp;
+            for (int j = 0; j < to - i; j++) {
+                if (array[j] > array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        return array;
     }
 }
---
+++ src/com/pb/smolianykova/hw4/CapitalLetter.java
@@ -0,0 +1,16 @@
+package com.pb.smolianykova.hw4;
+
+import java.util.Scanner;
+
+public class CapitalLetter {
+    static void print (String in) {
+        System.out.println(in);
+        }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите текст");
+
+    }
+}
+
+
