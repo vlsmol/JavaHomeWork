7f23846bf4a38f1047f3b89a355e7b163177abbc
Vladlena Smolianykova <vladlena.smolianykova@privatbank.ua>
2021-10-30 01:26:32 +0300

    init commit

--- src/com/pb/smolianykova/hw4/Anagram.java
+++ src/com/pb/smolianykova/hw4/Anagram.java
@@ -1,4 +1,48 @@
 package com.pb.smolianykova.hw4;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Anagram {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+
+            System.out.print("Enter first string: ");
+            String first = in.nextLine().toUpperCase();
+
+
+            System.out.print("Enter second string: ");
+            String second = in.nextLine().toUpperCase();
+
+            String result = isAnagram(first, second);
+            System.out.println(result);
+        }
+
+        private static String isAnagram(String first, String second) {
+            String answer = "";
+
+
+            if (first.matches("[A-Z\\.\\,\\s]")) {
+                String st = first.replaceAll("\\.\\,\\s", "");
+                String nd = second.replaceAll("\\.\\,\\s", "");
+                char[] arraySt = st.toCharArray();
+                char[] arrayNd = nd.toCharArray();
+                Arrays.sort(arraySt);
+                Arrays.sort(arrayNd);
+
+                if(Arrays.equals(arraySt, arrayNd)) {
+                    answer = "Anagram.";
+                }
+
+                else {
+                    answer = "No anagram.";
+                }
+            }
+
+            else  {
+                answer = "No anagram.";
+            }
+            return answer;
+        }
 }
+
