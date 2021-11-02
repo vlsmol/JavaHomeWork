8b9f16cf93e6bb2b5c788acda13f7af4ff472fe7
Vladlena Smolianykova <vladlena.smolianykova@privatbank.ua>
2021-11-02 21:18:32 +0200

    init commit

--- src/com/pb/smolianykova/hw5/Library.java
+++ src/com/pb/smolianykova/hw5/Library.java
@@ -1,4 +1,40 @@
 package com.pb.smolianykova.hw5;
 
 public class Library {
+    public static void main(String[] args) {
+        Reader reader1 = new Reader("Петров В.В.", 1, "ИФ", "21.10.1999", "+806711111");
+        Reader[] readers = {reader1};
+
+        Book book1 = new Book("Приключения", "Иванов И. И.");
+        Book book2 = new Book("Словарь", "Сидоров А. В.");
+        Book book3 = new Book("Энциклопедия", "Гусев К. В.");
+        Book[] books = {book1, book2, book3};
+
+        printReaders(readers);
+        printBooks(books);
+
+        reader1.takeBook(3);
+        reader1.takeBook("Приключения, Словарь, Энциклопедия");
+        reader1.takeBook(book1, book2,book3);
+
+        reader1.returnBook(3);
+        reader1.returnBook("Приключения (Иванов И. И. 2000 г.), Словарь (Сидоров А. В 1980 г.), Энциклопедия (Гусев К. В. 2010 г.)\"");
+        reader1.returnBook(book1, book2,book3);
+    }
+
+    private static void printBooks(Book[] books) {
+        System.out.println("Список книг:");
+        for (Book book : books) {
+            System.out.println(book.getInfo());
+        }
+        System.out.println();
+    }
+
+    private static void printReaders(Reader[] readers) {
+        System.out.println("Список читателей:");
+        for (Reader reader : readers) {
+            System.out.println( reader.getInfo());
+        }
+        System.out.println();
+    }
 }
--- src/com/pb/smolianykova/hw5/Reader.java
+++ src/com/pb/smolianykova/hw5/Reader.java
@@ -1,4 +1,110 @@
 package com.pb.smolianykova.hw5;
 
 public class Reader {
+    private String fio;
+    private int number;
+    private String faculty;
+    private String dob;
+    private String phone;
+
+    public Reader(String fio, int number, String faculty, String dob, String phone) {
+        this.fio = fio;
+        this.number = number;
+        this.faculty = faculty;
+        this.dob = dob;
+        this.phone = phone;
+    }
+
+    public Reader() {
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void takeBook(int number) {
+        System.out.println(this.fio + " взял " + number + " книги.");
+    }
+
+    public void takeBook(String... books) {
+        System.out.println(this.fio + " взял следующие книги:");
+        for (String book : books) {
+            System.out.println(book);
+        }
+        System.out.println();
+    }
+
+    public void takeBook(Book... books) {
+        System.out.println(this.fio + " взял следующие книги:");
+        for (Book book : books) {
+            System.out.println(book.getName() + ", автор - " + book.getAuthor());
+        }
+        System.out.println();
+    }
+
+    public void returnBook(int number) {
+        System.out.println(this.fio + " вернул " + number + " книги.");
+    }
+
+    public void returnBook(String... books) {
+        System.out.println(this.fio + " вернул следующие книги:");
+        for (String book : books) {
+            System.out.println(book);
+        }
+        System.out.println();
+    }
+
+    public void returnBook(Book... books) {
+        System.out.println(this.fio + " вернул следующие книги:");
+        for (Book book : books) {
+            System.out.println(book.getName() + ", автор - " + book.getAuthor());
+        }
+        System.out.println();
+    }
+
+    public String getInfo() {
+        return "{" +
+                "fio='" + fio + '\'' +
+                ", number=" + number +
+                ", faculty='" + faculty + '\'' +
+                ", dob='" + dob + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
