6d5bb78ac17ccdec0f2daa91157e628065839d42
Vladlena Smolianykova <vladlena.smolianykova@privatbank.ua>
2021-11-21 10:43:16 +0200

    init commit

---
+++ src/com/pb/smolianykova/hw9/FileNumbers.java
@@ -0,0 +1,80 @@
+package com.pb.smolianykova.hw9;
+
+import java.util.Random;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileNumbers {
+    public static void main(String[] args) throws Exception {
+
+        int size = 100;
+        String nrFilePath = "C:\\Users\\Lenovo\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\smolianykova\\hw9/numbers.txt";
+        String oddPath = "C:\\Users\\Lenovo\\IdeaProjects\\JavaHomeWork\\src\\com\\pb\\smolianykova\\hw9/odd-numbers.txt";
+
+        int[] array = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt((size-1)) + 1;
+        }
+
+        createNumbersFile(array, nrFilePath);
+        createOddNumbersFile(nrFilePath, oddPath, size);
+    }
+
+    public static void createNumbersFile(int [] Array, String file) {
+        printToFile_x10(Array, file);
+    }
+
+    public static void createOddNumbersFile(String file, String oddFile, int size) {
+        Path path = Paths.get(file);
+
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String lineFull = " ", line;
+            while((line = reader.readLine()) != null) {
+                lineFull = String.join(" ", lineFull, line);
+            }
+
+            int[] oddArray = toNumArray(lineFull, size);
+
+            printToFile_x10(oddArray, oddFile);
+
+        } catch (Exception ex) {
+            System.out.println("Error with file read: " + ex);
+        }
+    }
+
+    private static int[] toNumArray(String line, int size) { // конвертация строки в числовой массив
+        String [] split = line.split(" ");
+
+        int[] intArr = new int[size];
+        int x = 0;
+
+        for (String s : split) {
+            if (!s.equals("")) {
+                intArr[x] = Integer.parseInt(s);
+                if (intArr[x] % 2 == 0) {
+                    intArr[x] = 0;
+                }
+                x++;
+            }
+        }
+        return intArr;
+    }
+
+    private static void printToFile_x10(int [] Array, String file) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(file))) {
+            for (int i = 0; i < Array.length; i++) {
+                writer.write(String.valueOf(Array[i]));
+                if ((i + 1) % 10 == 0) {
+                    writer.write("\n");
+                } else {
+                    writer.write(" ");
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("Error with file write: " + ex);
+        }
+    }
+}
